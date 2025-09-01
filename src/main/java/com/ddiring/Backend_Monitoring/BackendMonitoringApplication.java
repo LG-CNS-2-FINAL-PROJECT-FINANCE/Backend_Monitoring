52b4e2f27af9ce811a93b4b9aca88e9e2a41fa76
dohy0103 <kdohy0103@gmail.com>
2025-09-01 17:44:22 +0900

    init commit

---
+++ src/main/java/com/ddiring/Backend_Monitoring/BackendMonitoringApplication.java
@@ -0,0 +1,13 @@
+package com.ddiring.Backend_Monitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendMonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendMonitoringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ddiring/Backend_Monitoring/BackendMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddiring.Backend_Monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
