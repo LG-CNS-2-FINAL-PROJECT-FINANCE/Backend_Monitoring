79afab876a0906c7b394b54c12a2b6d4b3c6b6c3
NamSangwon <nsw3265@naver.com>
2025-09-03 16:11:50 +0900

    Add #2 #3 #4 : 거래 이벤트 유형에 따라 스트림 분기

--- src/main/java/com/ddiring/Backend_Monitoring/service/FraudDetectionService.java
+++ src/main/java/com/ddiring/Backend_Monitoring/service/FraudDetectionService.java
@@ -1,8 +1,15 @@
 package com.ddiring.Backend_Monitoring.service;
 
+import com.ddiring.Backend_Monitoring.event.dto.trade.TradeEvent;
+import com.ddiring.Backend_Monitoring.event.dto.trade.TradeRequestAcceptedEvent;
+import com.ddiring.Backend_Monitoring.event.dto.trade.TradeRequestRejectedEvent;
 import lombok.RequiredArgsConstructor;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.kstream.Consumed;
 import org.apache.kafka.streams.kstream.KStream;
 import org.springframework.kafka.config.StreamsBuilderFactoryBean;
+import org.springframework.kafka.support.serializer.JsonSerde;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,5 +18,25 @@ public class FraudDetectionService {
     private final StreamsBuilderFactoryBean streamsBuilder;
 
     public void initFraudDetection() {
+        try {
+            StreamsBuilder builder = streamsBuilder.getObject();
+
+            // 1. 모든 이벤트를 TradeEvent 상위 클래스로 역직렬화
+            KStream<String, TradeEvent> events = builder.stream("TRADE", Consumed.with(Serdes.String(), new JsonSerde<>(TradeEvent.class)));
+
+            // 2. 이벤트 유형에 따라 스트림 분기 및 명확한 캐스팅
+            KStream<String, TradeRequestAcceptedEvent> acceptedEvents = events
+                    .filter((key, value) -> value instanceof TradeRequestAcceptedEvent)
+                    .mapValues((key, value) -> (TradeRequestAcceptedEvent) value);
+
+            KStream<String, TradeRequestRejectedEvent> rejectedEvents = events
+                    .filter((key, value) -> value instanceof TradeRequestRejectedEvent)
+                    .mapValues((key, value) -> (TradeRequestRejectedEvent) value);
+
+
+
+        } catch (Exception e) {
+            throw new RuntimeException("[FraudDetection] 이상 거래 탐지 설정 중 오류 발생");
+        }
     }
 }
