26608b0f6bcc2869688fda9c56e4b874d547f7b8
NamSangwon <nsw3265@naver.com>
2025-09-03 13:07:41 +0900

    Add #1 : Kafka Streams 활성화

--- src/main/java/com/ddiring/Backend_Monitoring/BackendMonitoringApplication.java
+++ src/main/java/com/ddiring/Backend_Monitoring/BackendMonitoringApplication.java
@@ -2,8 +2,10 @@ package com.ddiring.Backend_Monitoring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafkaStreams;
 
 @SpringBootApplication
+@EnableKafkaStreams
 public class BackendMonitoringApplication {
 
 	public static void main(String[] args) {
